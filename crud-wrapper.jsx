f76fc8ee82b7482b039206f9bd7c72e3e13457c1
Devin Witherspoon <devin@udacity.com>
2017-08-30 18:50:06 -0700

    set up state transitions for crud wrapper

--- crud-wrapper.jsx
+++ crud-wrapper.jsx
@@ -1,9 +1,74 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const states = {
+  PRESENTING: 'PRESENTING',
+  EDITING: 'EDITING',
+  COMMITING: 'COMMITING'
+};
+
+const actions = {
+  CANCEL: 'CANCEL',
+  CHANGE: 'CHANGE',
+  COMMIT: 'COMMIT',
+  FAIL: 'FAIL',
+  SUCCESS: 'SUCCESS'
+}
+
+const transitions = {
+  [states.PRESENTING]: {
+    [actions.CHANGE]: states.EDITING
+  },
+  [states.EDITING]: {
+    // internal state for has changed
+    [actions.CANCEL]: states.PRESENTING,
+    [actions.CHANGE]: state.EDITING,
+    [actions.COMMIT]: state.COMMITING
+  },
+  [states.COMMITING]: {
+    [actions.FAIL]: states.EDITING,
+    [actions.SUCCESS]: states.PRESENTING // should never get here since it will have deleted
+  }
+};
+
+function transition(state, transition) {
+  return transitions[state][transition] || state
+}
+
+
+function getNextState(state, action, nextValue) {
+  const {value, status} = state;
+  const nextStatus = transition(status, action);
+  switch (nextStatus) {
+    case [actions.PRESENTING]:
+      return {
+        status: nextStatus
+      };
+
+    case [actions.EDITING]:
+      return {
+        status: nextStatus,
+        value: nextValue
+      };
+
+    case [actions.COMMITING]:
+      return {
+        status: nextStatus,
+        value
+      };
+
+    default:
+      return state;
+  }
+}
+
 function withCrud(WrappedComponent) {
   return class ComponentWithCrud extends React.CreateComponent {
     static propTypes = {
-      // onSubmit, onUpdate, onDelete passed in from parent (don't really care about them here)
-      value: React.PropTypes.any,
-      valueComparitor: React.Proptypes.func
+      onDelete: PropTypes.func,
+      onSubmit: PropTypes.func,
+      onUpdate: PropTypes.func,
+      value: PropTypes.any,
       ..._.omit(WrappedComponent.propTypes, ['onChange', 'onSubmit', 'onCancel']),
     };
 
@@ -13,39 +78,64 @@ function withCrud(WrappedComponent) {
 
     constructor(props) {
       this.state = {
-        value: this.props.value
+        status: states.PRESENTING
       };
     }
 
-    // may be better for componentWillUpdate
-    componentWillReceiveProps(nextProps) {
-      if (this.props.value !== nextProps) {
-        this.setState({
-          value: nextProps.value
-        });
-      }
+    handleStart = () => {
+      this.handleChange(this.props.value);
     }
 
     handleChange = (nextValue) => {
-      this.setState({
-        value: nextValue
-      })
+      this.replaceState((state) => getNextState(state, actions.CHANGE, nextValue));
     }
 
-    handleCancel = () = {
-      this.setState({
-        value: this.props.value
-      })
+    handleCancel = () => {
+      this.replaceState((state) => getNextState(state, actions.CANCEL))
+    }
+
+    handleCommit = (commitFunc) => {
+      if (typeof commitFunc === 'function') {
+        const maybeCommitPromise = commitFunc(this.state.value);
+        if (maybeCommitPromise && maybeCommitPromise.then) {
+          maybeCommitPromise
+            .then(() => this.replaceState((state) => getNextState(state, actions.SUCCESS)))
+            .catch(() => this.replaceState((state) => getNextState(state, actions.FAIL)))
+        } else {
+          this.replaceState((state) => getNextState(state, actions.SUCCESS));
+        }
+      }
+    }
+
+    handleSubmit = () => {
+      this.handleCommit(this.props.onSubmit);
+    }
+
+    handleUpdate = () => {
+      this.handleCommit(this.props.onUpdate);
+    }
+
+    handleDelete = () => {
+      this.handleCommit(this.props.onDelete);
     }
 
     render() {
-      const {value} = this.state;
+      const {
+        props: {value: propsValue},
+        state: {value: stateValue, status, onSubmit, onUpdate, onDelete}
+      } = this;
+
       return (
         <WrappedComponent
-          {..._.omit(this.props, ['value', 'valueComparitor'])}
-          value={value}
-          onChange={this.handleChange}
+          {..._.omit(this.props, ['value'])}
+          status={status}
+          value={status === states.PRESENTING ? propsValue : stateValue}
+          onStart={this.handleStart}
           onCancel={this.handleCancel}
+          onChange={this.handleChange}
+          onSubmit={onSubmit ? this.handleSubmit : undefined}
+          onUpdate={onUpdate ? this.handleUpdate : undefined}
+          onDelete={onDelete ? this.handleDelete : undefined}
         />
       );
     }
