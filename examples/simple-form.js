d932cd4475d00c33b88110f6804d2f8aedcb6e09
Devin Witherspoon <devin@udacity.com>
2017-09-02 12:17:05 -0700

    simple form example

---
+++ examples/simple-form.js
@@ -0,0 +1,43 @@
+import Input from './input';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {states} from '../src/state-machine';
+import update from 'immutability-helper';
+
+export default class SimpleForm extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    status: PropTypes.oneOf(Object.keys(states)).isRequired,
+    value: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })).isRequired
+  }
+
+  handleChange = (inputValue, idx) => {
+    const {onChange, value} = this.props;
+    onChange(update(value, {
+      [idx]: {value: {$set: inputValue}}
+    }));
+  }
+
+  renderInputs() {
+    const {status, value} = this.props;
+    return value.map(({title, value: inputValue}, idx) =>
+      <li>
+        <span>{title}: </span>
+        <Input
+          onChange={(nextValue) => this.handleChange(nextValue, idx)}
+          value={inputValue}
+          status={status}
+        />
+      </li>
+    );
+  }
+
+  render() {
+    return <ul>
+      {this.renderInputs()}
+    </ul>;
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,6 @@
 import Input from '../examples/input';
 import React from 'react';
+import SimpleForm from '../examples/simple-form';
 import {compose} from 'lodash/fp';
 import {storiesOf} from '@storybook/react';
 import withButtons from '../examples/with-buttons';
@@ -15,10 +16,26 @@ const InputContainer = compose(
   withButtons('start', 'cancel', 'submit', 'delete')
 )(Input);
 
+const FormContainer = compose(
+  withState(true),
+  withCrud,
+  withButtons('start', 'cancel', 'submit', 'delete')
+)(SimpleForm);
+
 storiesOf('React CRUD Wrapper', module)
   .add('Simple Input', withInfo('Input wrapped in the CRUD decorator')(() => {
     return <InputWithCrud value='startValue' />;
   }))
-  .add('Simple Input With Buttons', withInfo('Input wrapped in the CRUD decorator')(() => {
+  .add('Input With Buttons', withInfo('Input wrapped in the CRUD decorator')(() => {
     return <InputContainer initialValue='startValue' />;
+  }))
+  .add('Simple Form With Buttons', withInfo('Input wrapped in the CRUD decorator')(() => {
+    return <FormContainer
+      initialValue={[
+        {title: 'First Name', value: ''},
+        {title: 'Last Name', value: ''},
+        {title: 'Email', value: ''},
+        {title: 'Password', value: ''},
+      ]}
+    />;
   }));
