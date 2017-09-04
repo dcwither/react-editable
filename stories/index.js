bdc09dd0b508561866223a377ccef40a8b596083
Devin Witherspoon <devin@udacity.com>
2017-09-04 12:49:57 -0700

    added actions to examples

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,6 @@ import {compose} from 'lodash/fp';
 import {storiesOf} from '@storybook/react';
 import withButtons from '../examples/with-buttons';
 import withEditable from '../src';
-import {withInfo} from '@storybook/addon-info';
 import withState from '../examples/with-state';
 
 const composeWithState = compose(
@@ -32,18 +31,18 @@ const billingInformaion = [
 
 storiesOf('ReactEditable', module)
   .addDecorator(Container)
-  .add('Input', withInfo('Input wrapped in the Editable decorator')(() => {
+  .add('Input', () => {
     const InputContainer = composeWithState(Input);
     return <InputContainer title='Input' initialValue='' />;
-  }))
-  .add('Form', withInfo('Form wrapped in the Editable decorator')(() => {
+  })
+  .add('Form', () => {
     const FormContainer = composeWithState(Form);
     return <FormContainer
       title='Account Information'
       initialValue={accountInformation}
     />;
-  }))
-  .add('Multi-Form', withInfo('Multi-Form in the Editable decorator')(() => {
+  })
+  .add('Multi-Form', () => {
     const MultiFormContainer = composeWithState(MultiForm);
     return <MultiFormContainer
       title='Sign Up'
@@ -52,8 +51,8 @@ storiesOf('ReactEditable', module)
         {title: 'Billing', value: billingInformaion},
       ]}
     />;
-  }))
-  .add('Form With Dynamic Fields', withInfo('Form with dynamic fields wrapped in the Editable decorator')(() => {
+  })
+  .add('Form With Dynamic Fields', () => {
     const DynamicFieldFormContainer = composeWithState(DynamicFieldForm);
     return <DynamicFieldFormContainer initialValue={[{title: 'Field 1', value: ''}]} />;
-  }));
+  });
