507748e284f45f8bc394b984be3fabdd0a486996
Devin Witherspoon <devin@udacity.com>
2018-04-25 20:39:37 -0700

    cleaned up some type issues

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  roots: ["<rootDir>/src"],
-  setupFiles: ["raf/polyfill", "./jest.setup.ts"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  transform: { "^.+\\.tsx?$": "ts-jest" }
-};
