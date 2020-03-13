d67806bf09e556f29d78fb404fd74d3ae47ef61f
Devin Witherspoon <dcwither@gmail.com>
2020-03-12 20:48:39 -0700

    add tests and fix set state relative bug

--- src/use-editable.spec.tsx
+++ src/use-editable.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import useEditable from "./use-editable";
 
 describe("useEditable", () => {
@@ -17,5 +17,136 @@ describe("useEditable", () => {
         "value": "INITIAL_VALUE",
       }
     `);
+  });
+
+  it("should switch to EDITING onChange", () => {
+    const { result } = renderHook(() =>
+      useEditable({ value: "INITIAL_VALUE" })
+    );
+    act(() => {
+      result.current.onStart();
+    });
+    expect(result.current).toMatchInlineSnapshot(`
+      Object {
+        "onCancel": [Function],
+        "onChange": [Function],
+        "onCommit": [Function],
+        "onStart": [Function],
+        "status": "EDITING",
+        "value": "INITIAL_VALUE",
+      }
+    `);
+  });
+
+  it("should switch to EDITING with NEW_VALUE onChange", () => {
+    const { result } = renderHook(() =>
+      useEditable({ value: "INITIAL_VALUE" })
+    );
+    act(() => {
+      result.current.onChange("NEW_VALUE");
+    });
+    expect(result.current).toMatchInlineSnapshot(`
+      Object {
+        "onCancel": [Function],
+        "onChange": [Function],
+        "onCommit": [Function],
+        "onStart": [Function],
+        "status": "EDITING",
+        "value": "NEW_VALUE",
+      }
+    `);
+  });
+
+  it("should reset state onCancel", () => {
+    const onCancel = jest.fn();
+    const { result } = renderHook(() =>
+      useEditable({ value: "INITIAL_VALUE", onCancel })
+    );
+    act(() => {
+      result.current.onChange("NEW_VALUE");
+    });
+    act(() => {
+      result.current.onCancel();
+    });
+
+    expect(onCancel).toHaveBeenCalledWith("NEW_VALUE");
+    expect(result.current).toMatchInlineSnapshot(`
+      Object {
+        "onCancel": [Function],
+        "onChange": [Function],
+        "onCommit": [Function],
+        "onStart": [Function],
+        "status": "PRESENTING",
+        "value": "INITIAL_VALUE",
+      }
+    `);
+  });
+
+  describe("when onCommit is synchronous", () => {
+    it("should call onCommit with NEW_VALUE onCommit and reset state", () => {
+      const onCommit = jest.fn();
+      const { result } = renderHook(() =>
+        useEditable({ value: "INITIAL_VALUE", onCommit })
+      );
+      act(() => {
+        result.current.onChange("NEW_VALUE");
+      });
+      act(() => {
+        result.current.onCommit("SUBMIT", result.current.value);
+      });
+
+      expect(onCommit).toHaveBeenCalledWith("SUBMIT", "NEW_VALUE");
+      expect(result.current).toMatchInlineSnapshot(`
+        Object {
+          "onCancel": [Function],
+          "onChange": [Function],
+          "onCommit": [Function],
+          "onStart": [Function],
+          "status": "PRESENTING",
+          "value": "INITIAL_VALUE",
+        }
+      `);
+    });
+  });
+
+  describe("when onCommit returns a Promise", () => {
+    it("should call onCommit with NEW_VALUE onCommit and reset state after promise resolves", async () => {
+      const promise = Promise.resolve();
+      const onCommit = jest.fn(() => promise);
+      const { result } = renderHook(() =>
+        useEditable({ value: "INITIAL_VALUE", onCommit })
+      );
+      act(() => {
+        result.current.onChange("NEW_VALUE");
+      });
+      act(() => {
+        result.current.onCommit("SUBMIT", result.current.value);
+      });
+
+      expect(onCommit).toHaveBeenCalledWith("SUBMIT", "NEW_VALUE");
+      expect(result.current).toMatchInlineSnapshot(`
+        Object {
+          "onCancel": [Function],
+          "onChange": [Function],
+          "onCommit": [Function],
+          "onStart": [Function],
+          "status": "COMMITTING",
+          "value": "NEW_VALUE",
+        }
+      `);
+      await act(async () => {
+        await promise;
+      });
+      expect(result.current).toMatchInlineSnapshot(`
+Object {
+  "onCancel": [Function],
+  "onChange": [Function],
+  "onCommit": [Function],
+  "onStart": [Function],
+  "status": "PRESENTING",
+  "value": "INITIAL_VALUE",
+}
+`);
+    });
   });
 });
