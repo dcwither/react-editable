eeed8dad4b6f06d79de7f9db9eb293f233f65bb3
Devin Witherspoon <dcwither@gmail.com>
2020-03-18 19:43:53 -0700

    test edge cases of editable context

--- src/editable-context.spec.tsx
+++ src/editable-context.spec.tsx
@@ -78,6 +78,34 @@ describe("Editable Context", () => {
     `);
   });
 
+  it("resets after onCancel is triggered", () => {
+    const handleCancel = jest.fn();
+    const { container } = render(
+      <Editable value="INITIAL_VALUE" onCancel={handleCancel}>
+        <TestHarness />
+      </Editable>
+    );
+
+    fireEvent.change(getByTestId(container, "input"), {
+      target: { value: "NEW_VALUE" }
+    });
+    fireEvent.click(getByText(container, /Cancel/));
+    expect(handleCancel).toHaveBeenCalledWith("NEW_VALUE");
+    expect(getByTestId(container, "form")).toMatchInlineSnapshot(`
+      <div
+        data-testid="form"
+      >
+        <p>
+          PRESENTING
+        </p>
+        <input
+          data-testid="input"
+          value="INITIAL_VALUE"
+        />
+      </div>
+    `);
+  });
+
   describe("synchronous commits", () => {
     it("triggers the commit when clicking Submit", () => {
       const handleCommit = jest.fn();
@@ -109,7 +137,7 @@ describe("Editable Context", () => {
   });
 
   describe("asynchronous commits", () => {
-    it("it transitions to COMMITING then PRESENTING when onCommit returns a promise", async () => {
+    it("transitions to COMMITING then PRESENTING when onCommit returns a promise", async () => {
       const promise = Promise.resolve();
       const handleCommit = jest.fn(() => promise);
       const { container } = render(
@@ -149,6 +177,75 @@ describe("Editable Context", () => {
           <input
             data-testid="input"
             value="INITIAL_VALUE"
+          />
+        </div>
+      `);
+    });
+
+    it("doesn't error when component unmounts during commit", async () => {
+      const promise = Promise.resolve();
+      const handleCommit = jest.fn(() => promise);
+      jest.spyOn(console, "error");
+      const { container, unmount } = render(
+        <Editable value="INITIAL_VALUE" onCommit={handleCommit}>
+          <TestHarness />
+        </Editable>
+      );
+
+      fireEvent.change(getByTestId(container, "input"), {
+        target: { value: "NEW_VALUE" }
+      });
+      fireEvent.click(getByText(container, /Submit/));
+
+      unmount();
+      await act(async () => {
+        await promise;
+      });
+      expect(console.error).not.toHaveBeenCalled();
+      expect(container).toMatchInlineSnapshot(`<div />`);
+    });
+
+    it("transitions to COMMITING then EDITING when onCommit rejects a promise", async () => {
+      const promise = Promise.reject();
+      const handleCommit = jest.fn(() => promise);
+      const { container } = render(
+        <Editable value="INITIAL_VALUE" onCommit={handleCommit}>
+          <TestHarness />
+        </Editable>
+      );
+
+      fireEvent.change(getByTestId(container, "input"), {
+        target: { value: "NEW_VALUE" }
+      });
+      fireEvent.click(getByText(container, /Submit/));
+      expect(handleCommit).toHaveBeenCalledWith("SUBMIT", "NEW_VALUE");
+      expect(getByTestId(container, "form")).toMatchInlineSnapshot(`
+        <div
+          data-testid="form"
+        >
+          <p>
+            COMMITTING
+          </p>
+          <input
+            data-testid="input"
+            value="NEW_VALUE"
+          />
+        </div>
+      `);
+
+      await act(async () => {
+        await promise.catch(() => null);
+      });
+      expect(getByTestId(container, "form")).toMatchInlineSnapshot(`
+        <div
+          data-testid="form"
+        >
+          <p>
+            EDITING
+          </p>
+          <input
+            data-testid="input"
+            value="NEW_VALUE"
           />
         </div>
       `);
